826d2812bff5f959889446e6900277e8fd691eb3
rene <renegmed@yahoo.com>
2018-07-27 22:43:04 -0400

    05 calculating map latitude and longitude

--- source/components/map.js
+++ source/components/map.js
@@ -3,18 +3,24 @@ import React, {Component} from 'react';
 export default class Map extends Component {
     onClick(e) {
         let img = e.target,
-            point = {x:e.offsetX, y:e.offsetY};
+            point = {x:e.offsetX, y:e.offsetY},
+            latSize = img.height/180,
+            lonSize = img.width/360,
+            lat, lon;
 
-        console.log(point);
+        //console.log(point);
 
-        if (point.x) {
+        if (!point.x) {
             point = {
                   x:e.pageX - e.target.offsetLeft,
                   y:e.pageY - e.target.offsetTop
             };                  
         }
 
-        console.log(point);
+        lat = (point.y - (img.height/2)) / -latSize;
+        lon = (point.x - (img.width/2)) / lonSize;
+
+        console.log(lat, lon);
     }
     
     render() {
